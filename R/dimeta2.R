89d13d90a4996d4b01348134582a44eeec4dce36
Nicolas Robette <nicolas.robette@uvsq.fr>
2014-09-05 00:00:00 +0000

    version 1.3

--- R/dimeta2.R
+++ R/dimeta2.R
@@ -1,9 +1,12 @@
-dimeta2 <- function(resmca,l,n,dim=1:resmca$call$ncp) {
+dimeta2 <- function(resmca,l,n=names(l),dim=1:resmca$call$ncp) {
   eta2 <- matrix(nrow=length(l),ncol=length(dim))
   sub <- rep(TRUE,times=nrow(resmca$ind$coord))
   if(class(resmca)[1]=='csMCA') sub <- resmca$call$subcloud
+  if(class(resmca)[1]=='stMCA') if(resmca$call$input.mca=='csMCA') sub <- resmca$call$subcloud #new
+  ww <- resmca$call$row.w[sub] #new
+  if(class(resmca)[1]=='stMCA') ww <- resmca$call$fit$weights #new
   for(i in 1:length(dim)) {
-    for(j in 1:length(l)) eta2[j,dim[i]] <- summary(lm(resmca$ind$coord[,dim[i]]~l[[j]][sub],weights=resmca$call$row.w[sub]))$r.squared
+    for(j in 1:length(l)) eta2[j,dim[i]] <- summary(lm(resmca$ind$coord[,dim[i]]~l[[j]][sub],weights=ww))$r.squared
    }
   rownames(eta2) <- n
   colnames(eta2) <- colnames(resmca$var$contrib)[dim]
